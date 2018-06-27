13d3b7cd2ab55688aac2719473c423a2749625e6
marc borkowski <borkowskiberlin@gmail.com>
2018-06-27 15:49:35 +0200

    Listing the projects

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Projects from './Components/Projects'
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      projects: []
+    }
+  }
+
+  componentWillMount() {
+    this.setState({projects: [
+      {
+        title: 'Verpflegung bereitstellen',
+        category: 'Zufriedenheit'
+      },
+      {
+        title: 'Arbeitsplatz einrichten',
+        category: 'Effizents'
+      },
+      {
+        title: 'Gespräch führen',
+        category: 'Feedback'
+      }
+    ]});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
